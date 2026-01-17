140ec07729b9f0b48179bd9cf49a63e2eb91bd85
hynseok <wefwef12e@naver.com>
2026-01-17 21:48:35 +0900

    fix sort param name

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -178,11 +178,13 @@ async fn search_papers(
     }
 
     // Sorting
+    if let Some(ref s) = params.sort {
         match s.as_str() {
             "year" => main_search.with_sort(&["year:desc"]),
-            "citation_count_desc" => main_search.with_sort(&["citation_count:desc"]),
+            "citation_count" => main_search.with_sort(&["citation_count:desc"]),
             _ => &mut main_search,
         };
+    }
     
     // Enable Highlighting
     main_search.with_attributes_to_highlight(Selectors::Some(&["title", "venue", "authors"]));
