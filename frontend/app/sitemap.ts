6917024e1191f40d33733304a4ad70c634bb499e
hynseok <wefwef12e@naver.com>
2026-04-04 13:13:20 +0900

    cache disable

--- frontend/app/sitemap.ts
+++ frontend/app/sitemap.ts
@@ -1,6 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'
